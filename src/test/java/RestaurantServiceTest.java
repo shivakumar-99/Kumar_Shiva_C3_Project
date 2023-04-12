615a4db4bf91e7b03ff08901ba9d236f654adbed
shivakumar-99 <shivakumarchary799@gmail.com>
2023-04-12 14:07:55 +0530

    Part 2: Solution

---
+++ src/test/java/RestaurantServiceTest.java
@@ -0,0 +1,70 @@
+import org.junit.jupiter.api.*;
+
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestaurantServiceTest {
+
+    static RestaurantService service = new RestaurantService();
+    static Restaurant restaurant;
+    //REFACTOR ALL THE REPEATED LINES OF CODE
+    @BeforeAll
+    static void initAll() {
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant = service.addRestaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+        System.out.println("Initialisation Done");
+    }
+
+    @AfterAll
+    static void tearDown() {
+        service = null;
+        restaurant = null;
+        System.out.println("Tear Down Completed");
+    }
+    //>>>>>>>>>>>>>>>>>>>>>>SEARCHING<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    @Test
+    public void searching_for_existing_restaurant_should_return_expected_restaurant_object() throws restaurantNotFoundException {
+        //WRITE UNIT TEST CASE HERE
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        Restaurant restaurant3 = service.addRestaurant("A","Chennai",openingTime,closingTime);
+
+        String existingRestaurant = restaurant.getName();
+        assertEquals(restaurant3, service.findRestaurantByName("A"));
+    }
+
+    //You may watch the video by Muthukumaran on how to write exceptions in Course 3: Testing and Version control: Optional content
+    @Test
+    public void searching_for_non_existing_restaurant_should_throw_exception() throws restaurantNotFoundException {
+        //WRITE UNIT TEST CASE HERE
+        String non_existing_restaurant = "not a valid restaurant";
+        assertThrows(restaurantNotFoundException.class,()->{service.findRestaurantByName(non_existing_restaurant);});
+    }
+    //<<<<<<<<<<<<<<<<<<<<SEARCHING>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    //>>>>>>>>>>>>>>>>>>>>>>ADMIN: ADDING & REMOVING RESTAURANTS<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    @Test
+    public void remove_restaurant_should_reduce_list_of_restaurants_size_by_1() throws restaurantNotFoundException {
+        int initialNumberOfRestaurants = service.getRestaurants().size();
+        service.removeRestaurant("Amelie's cafe");
+        assertEquals(initialNumberOfRestaurants-1, service.getRestaurants().size());
+    }
+
+    @Test
+    public void removing_restaurant_that_does_not_exist_should_throw_exception() throws restaurantNotFoundException  {
+        assertThrows(restaurantNotFoundException.class,()->service.removeRestaurant("Pantry d'or"));
+    }
+
+    @Test
+    public void add_restaurant_should_increase_list_of_restaurants_size_by_1(){
+        int initialNumberOfRestaurants = service.getRestaurants().size();
+        service.addRestaurant("Pumpkin Tales","Chennai",LocalTime.parse("12:00:00"),LocalTime.parse("23:00:00"));
+        assertEquals(initialNumberOfRestaurants + 1,service.getRestaurants().size());
+    }
+
+    //<<<<<<<<<<<<<<<<<<<<ADMIN: ADDING & REMOVING RESTAURANTS>>>>>>>>>>>>>>>>>>>>>>>>>>
+}
